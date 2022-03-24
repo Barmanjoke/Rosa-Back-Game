c051ca81731a8ffbb6f9eab0822ccb33d6168c3a
Aristide <aristide.cuny@epita.fr>
2022-03-24 12:41:43 +0100

    achievements & db

--- gamification/settings.py
+++ gamification/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'achievements'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'gamification.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'rosa',
+        'USER':'backend-games',
+        'PASSWORD':'c0viZ0g95eihmir39xYC',
+        'HOST':'rosa-central-backend.postgres.database.azure.com',
+        'PORT':'5432',
     }
 }
 
